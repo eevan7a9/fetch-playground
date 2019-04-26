71570b8903ca0092ed50501603ed45d0661139b7
eevan7a9 <e7a9@protonmail.com>
2019-04-26 22:27:52 +0900

    fetch POST

--- js/main.js
+++ js/main.js
@@ -2,8 +2,8 @@ document.querySelector("#header").innerHTML = "Fetch Text, JSON, API";
 // declare variables
 const getTxt = document.querySelector("#btnGetTxt");
 const getJson = document.querySelector("#btnGetJson");
-const getApi = document.querySelector('#btnGetApi');
-const btnReset = document.querySelector('#btnReset');
+const getApi = document.querySelector("#btnGetApi");
+const btnReset = document.querySelector("#btnReset");
 
 const textField = document.querySelector("#textField");
 
@@ -12,6 +12,7 @@ function init() {
   getJson.addEventListener("click", showJson);
   getApi.addEventListener("click", showApi);
   btnReset.addEventListener("click", resetAll);
+  btnSubmit.addEventListener("click", addPost);
   textField.value = ""; /** empty the value */
 }
 
@@ -23,7 +24,7 @@ const showText = () => {
       textField.value = data;
     })
     .catch(err => console.log(err));
-    document.querySelector('#targetDiv').innerHTML = "";
+  document.querySelector("#targetDiv").innerHTML = "";
 };
 
 const showJson = () => {
@@ -31,7 +32,8 @@ const showJson = () => {
   fetch("avengers.json")
     .then(res => res.json())
     .then(data => {
-      let listsHero = ""; /** this will hold the data before we show to the DOM */
+      let listsHero =
+        ""; /** this will hold the data before we show to the DOM */
       let avengers = data;
       avengers.forEach(avenger => {
         listsHero += `
@@ -44,17 +46,18 @@ const showJson = () => {
            `;
       });
       document.querySelector("#targetDiv").innerHTML = listsHero;
-    }).catch((err) => console.log(err));
+    })
+    .catch(err => console.log(err));
 };
 
 const showApi = () => {
-    fetch("https://jsonplaceholder.typicode.com/posts").then((res) => res.json())
-    .then((data) => {
-        let listsPosts = ""
-        let posts = data;
-        posts.forEach(post => {
-            listsPosts += 
-            `
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => {
+      let listsPosts = "";
+      let posts = data;
+      posts.forEach(post => {
+        listsPosts += `
                 <div class="card mt-2 mb-2">
                     <div class="card-body">
                         <small>ID :${post.id}</small>
@@ -62,14 +65,37 @@ const showApi = () => {
                         <p>${post.body}</p>
                     </div>
                 </div> 
-            `
-        });
-        document.querySelector('#targetDiv').innerHTML = listsPosts;
-    })
-}
+            `;
+      });
+      document.querySelector("#targetDiv").innerHTML = listsPosts;
+    });
+};
+
+const addPost = event => {
+  event.preventDefault();
+
+  let title = document.querySelector("#title").value;
+  let body = document.querySelector("#body").value;
+
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    headers: {
+      Accept: "Application/json, text/SecurityPolicyViolationEvent. */*",
+      "content-type": "application/json",
+    },
+    body: JSON.stringify({
+      title: title,
+      body: body,
+    }),
+  })
+    .then(res => res.json())
+    .then(data => console.log(data));
+};
 
 const resetAll = () => {
-    textField.value = "";
-    document.querySelector('#targetDiv').innerHTML = "";
-}
+  textField.value = "";
+  document.querySelector("#targetDiv").innerHTML = "";
+  document.querySelector("#title").value = "";
+  document.querySelector("#body").value = "";
+};
 init();
